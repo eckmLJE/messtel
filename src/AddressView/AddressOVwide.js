5e1a12f2a155725986f98ed87491f6f72f8573d9
eckmLJE <lucas.eckman@gmail.com>
2018-11-17 15:55:46 -0500

    more wide view layout

--- src/AddressView/AddressOVwide.js
+++ src/AddressView/AddressOVwide.js
@@ -5,19 +5,41 @@ import CommentsList from "../Comments/CommentsList";
 
 const AddressOVwide = props => (
   <div className="address-ov-wide">
-    <div className="address-stats">
-      <ul>
-        <li>Last Updated:</li>
-        <li>Notes:</li>
-      </ul>
-    </div>
-    <div className="address-data">
-      <ul>
-        <li>Loading Dock Req:</li>
-        <li>Loading Loc:</li>
-        <li>Security Rating:</li>
-        <li>Cutoff Time:</li>
-      </ul>
+    <div className="address-info">
+      <div className="address-stats">
+        <ul>
+          <li>
+            <span>Last Updated:</span>
+            <span>{props.lastUpdated}</span>
+          </li>
+          <li>
+            <span>Notes:</span>
+            <span>{props.comments.length}</span>
+          </li>
+        </ul>
+      </div>
+      <div className="address-data">
+        <ul>
+          <li>
+            <span>Loading Dock Req:</span>
+            <span>Yes</span>
+            <span>95%</span>
+          </li>
+          <li>
+            <span>Loading Loc:</span>
+            <span>Rear Alley</span>
+            <span>75%</span>
+          </li>
+          <li>
+            <span>Security Rating:</span>
+            <span>3/5</span>
+            <span>51%</span>
+          </li>
+          <li>
+            <span>Cutoff Time:</span>
+          </li>
+        </ul>
+      </div>
     </div>
     <div className="address-comments">
       <CommentsList comments={props.comments} />
